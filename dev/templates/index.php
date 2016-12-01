029348f74fb6504974b29acb0fa16b413451e320
John Ruane <john.ruane@jdwilliams.co.uk>
2016-12-01 16:46:46 +0000

    created 2 arrays to count and store links

--- dev/templates/index.php
+++ dev/templates/index.php
@@ -5,27 +5,38 @@
 
 $arr = ['ageless','ample','assets','boob','braless','bust','busty','cleavage','enviable','eye-popping','figure-hugging','flashes','flashing','flaunt','flaunts','gushes','gym','leggy','midriff','perky','pert','plunging','postirior','racy','revealing','scantly','scanty','sexy','showcase','showcases','skimpy','sideboob','sizable','sizzle','sizzling','skimpy','skin-tight','slim','steamy','thigh','toned','underboob','yummy'];
 $matchedanchors = [];
+$frequencey = [];
 
 $html = file_get_contents('http://www.dailymail.co.uk/');
 $dom = new DOMDocument;
 $dom->loadHTML($html);
+
+// foreach($arr as $a) {
+//     if (strcasecmp($n, $a) == 0 && $element->getAttribute('href') != "#") {
+//         array_push($matchedanchors, $n . ';' . $element->getAttribute('href'));
+//     }
+// }
 
 foreach($dom->getElementsByTagName('a') as $element) {
     $node_ary = explode(' ', trim($element->nodeValue.'<br />'));
     if (is_array($node_ary)) {
         foreach($node_ary as $n) {
             foreach($arr as $a) {
-                if (strcasecmp($n, $a) == 0) {
-                    // array_push($foundwords, $n);
-                    $matchedanchors[$n] = $element->getAttribute('href');
+                if (strcasecmp($n, $a) == 0 && $element->getAttribute('href') != "#") {
+                    array_push($frequencey, strtolower($n));
+                    if (array_key_exists($n, $matchedanchors)) {
+                        $matchedanchors[$n] = $matchedanchors[$n] . ';' . $element->getAttribute('href');
+                    } else {
+                        $matchedanchors[$n] = $element->getAttribute('href');
+                    }
                 }
             }
         }
     }
 }
-
-// $sortwords = array_count_values(array_map('strtolower', $foundwords));
-// arsort($sortwords);
+array_change_key_case($matchedanchors, CASE_LOWER);
+$frequencycount = array_count_values($frequencey);
+arsort($frequencycount);
 ?>
 
 <!doctype html>
@@ -51,9 +62,9 @@ foreach($dom->getElementsByTagName('a') as $element) {
     </div> -->
     <div class="content-wrapper">
         <?php
-            while (list($key, $val) = each($matchedanchors)) {
-                echo "<p><span>$key: $val</span></p>";
-            }
+            // while (list($key, $val) = each($matchedanchors)) {
+            //     echo "<p><span>$key: $val</span></p>";
+            // }
         ?>
     </div>
 </body>
