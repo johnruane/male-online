bae07bf5b9165ba020c3a248c4b396872d6edd40
John Ruane <johnmruane@gmail.com>
2016-12-06 23:44:30 +0000

    both arrays work. output works. needs styling better

--- build/index.php
+++ build/index.php
@@ -2,32 +2,87 @@
     // ini_set('display_errors', 'On');
     // ini_set('html_errors', 0);
 
-    $arr = ['ageless','ample','assets','boob','braless','bust','busty','cleavage','enviable','eye-popping','figure-hugging','flashes','flashing','flaunt','flaunts','gushes','gym','leggy','midriff','perky','pert','plunging','postirior','racy','revealing','scantly','scanty','sexy','showcase','showcases','skimpy','sideboob','sizable','sizzle','sizzling','skimpy','skin-tight','slim','steamy','thigh','toned','underboob','yummy'];
-    $matchedanchors = [];
-    $frequencey = [];
+    // $bad_words = ['ageless','ample','assets','boob','braless','bust','busty','cleavage','enviable','eye-popping','figure-hugging','flashes','flashing','flaunt','flaunts','gushes','gym','leggy','midriff','perky','pert','plunging','postirior','racy','revealing','scantly','scanty','sexy','showcase','showcases','skimpy','sideboob','sizable','sizzle','sizzling','skimpy','skin-tight','slim','steamy','thigh','toned','underboob','yummy'];
+    $bad_words = ['leggy'];
+    $article_results = array();
+    $frequencey = array();
 
-    $html = file_get_contents('http://mailonline.co.uk');
+    //$html = file_get_contents('http://mailonline.co.uk');
+    $html = file_get_contents('mail.html');
     $dom = new DOMDocument;
     $dom->loadHTML($html);
+    $xpath = new DomXpath($dom);
 
-    foreach($dom->getElementsByTagName('a') as $element) {
-        $node_ary = explode(' ', trim($element->nodeValue.'<br />'));
-        foreach($node_ary as $n) {
-            foreach($arr as $a) {
-                if (strcasecmp($n, $a) == 0 && $element->getAttribute('href') != "#") {
-                    array_push($frequencey, strtolower($n));
-                    if (array_key_exists($n, $matchedanchors)) {
-                        array_push($matchedanchors[$n], $element);
-                    } else {
-                        $matchedanchors[$n][] = $element;
-                    }
-                }
-            }
-        }
+    $articles = $xpath->query('//div[contains(concat(" ", normalize-space(@class), " "), " article ")]');
+    $article_results = array();
+    $frequencey = array();
+
+    foreach ($articles as $article) {
+    	$node = $xpath->query("descendant::p | descendant::a", $article);
+    	$node_text = $node->item(0)->textContent;
+
+    	foreach ($bad_words as $word) {
+    		if ( stripos($node_text, ' ' . $word . ' ') !== false ) {
+
+    	 		array_push($frequencey, strtolower($word));
+
+                $node = $xpath->query("descendant::a", $article);
+    			$result['main'] = $node->item(0)->textContent;
+
+                $node = $xpath->query("descendant::a/attribute::href", $article);
+                $result['link'] = $node->item(0)->nodeValue;
+
+    			// $node = $xpath->query("descendant::p", $article);
+    		    // $result['p'] = $node->item(0)->textContent;
+
+    			$node = $xpath->query("descendant::img/attribute::data-src", $article);
+    			$result['img'] = $node->item(0)->nodeValue;
+
+    			if ( array_key_exists ( $word , $article_results ) ) {
+    				array_push($article_results[$word], $result);
+    			} else {
+    				$article_results[$word][] = $result;
+    			}
+    		}
+    	}
     }
-    array_change_key_case($matchedanchors, CASE_LOWER);
+
+    $list_articles = $xpath->query('//div[contains(concat(" ", normalize-space(@class), " "), "femail")]//li');
+    $list_results = array();
+
+    foreach ($list_articles as $article) {
+
+    	$node = $xpath->query('descendant::span[@class="pufftext"]', $article);
+    	$node_text = $node->item(0)->textContent;
+
+    	foreach ($bad_words as $word) {
+
+    		if ( stripos($node_text, $word) !== false ) {
+    			array_push($frequencey, strtolower($word));
+
+    			$result['main'] = $node->item(0)->textContent;
+
+    			$node = $xpath->query('descendant::a/attribute::href', $article);
+    			$result['link'] = $node->item(0)->nodeValue;
+
+    			$node = $xpath->query('descendant::img/attribute::data-src', $article);
+    			$result['img'] = $node->item(0)->nodeValue;
+
+    			if ( array_key_exists ( $word , $list_results ) ) {
+    				array_push($list_results[$word], $result);
+    			} else {
+    				$list_results[$word][] = $result;
+    			}
+    		}
+    	}
+    }
+
+
+    $merge_results = array_merge($article_results, $list_results);
+
     $frequencycount = array_count_values($frequencey);
     arsort($frequencycount);
+
 ?>
 
 <!doctype html>
@@ -56,9 +111,8 @@
                 <p onClick="openLinks('<?php echo $fkey; ?>')"><span><?php echo $fkey . " " . $fvalue; ?></span></p>
                 <div id="<?php echo $fkey; ?>" class="keyword-wrapper">
                     <ul class="article-list">
-                        <?php foreach($matchedanchors[$fkey] as $mkey => $mvalue): ?>
-                            <li><a href="<?php echo $mvalue->getAttribute('href'); ?>">
-                            <?php echo $mvalue->textContent; ?></a></li>
+                        <?php foreach($merge_results[$fkey] as $mkey => $value): ?>
+                            <li><a href="<?php echo $value['href'] ?>"><img src="<?php echo $value['img'] ?>"><p><?php echo $value['main'] ?></p></a></li>
                         <?php endforeach; ?>
                     </ul>
                 </div>
--- build/test.php
+++ build/test.php
@@ -4,7 +4,7 @@ ini_set('display_errors', 1);
 error_reporting(E_ALL ^ E_NOTICE);
 
 //$bad_words = ['sues','drinks'];
-$bad_words = ['history'];
+$bad_words = ['history','doing'];
 
 $html = file_get_contents('mail.html');
 $doc = new DomDocument();
@@ -15,64 +15,66 @@ $articles = $xpath->query('//div[contains(concat(" ", normalize-space(@class), "
 $article_results = array();
 $frequencey = array();
 
-foreach ($articles as $article) {
-	$node = $xpath->query("descendant::p | descendant::a", $article);
-	$node_text = $node->item(0)->textContent;
-
-	print_r($node_text);
-
-	foreach ($bad_words as $word) {
-		if ( stripos($node_text, $word) !== false ) {
-
-	 		array_push($frequencey, strtolower($word));
-
-			$result['a'] = $node->item(0)->textContent;
-
-			$node = $xpath->query("descendant::p", $article);
-		    $result['p'] = $node->item(0)->textContent;
-
-			$node = $xpath->query("descendant::img/attribute::src", $article);
-			$result['img'] = $node->item(0)->nodeValue;
-
-			if ( array_key_exists ( $word , $article_results ) ) {
-				array_push($list_results[$word], $result);
-			} else {
-				$article_results[$word] = $result;
-			}
-		}
-	}
-}
-
-print_r($article_results);
-
-$list_articles = $xpath->query('//div[@class="femail"]//li');
-$list_results = array();
-
-// foreach ($list_articles as $article) {
-//
-// 	$node = $xpath->query('descendant::span[@class="pufftext"]', $article);
+// foreach ($articles as $article) {
+// 	$node = $xpath->query("descendant::p | descendant::a", $article);
 // 	$node_text = $node->item(0)->textContent;
+//
+// 	print_r($node_text);
 //
 // 	foreach ($bad_words as $word) {
 // 		if ( stripos($node_text, $word) !== false ) {
-// 			array_push($frequencey, strtolower($word));
 //
-// 			$result['span'] = $node->item(0)->textContent;
+// 	 		array_push($frequencey, strtolower($word));
 //
-// 			$node = $xpath->query('descendant::a/attribute::href', $l_article);
-// 			$result['a_href'] = $node->item(0)->value;
+// 			$result['a'] = $node->item(0)->textContent;
 //
-// 			$node = $xpath->query('descendant::img/attribute::src', $l_article);
-// 			$result['img_src'] = $node->item(0)->value;
+// 			$node = $xpath->query("descendant::p", $article);
+// 		    $result['p'] = $node->item(0)->textContent;
 //
-// 			if ( array_key_exists ( $word , $list_results ) ) {
+// 			$node = $xpath->query("descendant::img/attribute::src", $article);
+// 			$result['img'] = $node->item(0)->nodeValue;
+//
+// 			if ( array_key_exists ( $word , $article_results ) ) {
 // 				array_push($list_results[$word], $result);
 // 			} else {
-// 				$list_results[$word] = $result;
+// 				$article_results[$word] = $result;
 // 			}
 // 		}
 // 	}
 // }
+//
+// print_r($article_results);
+
+$list_articles = $xpath->query('//div[@class="femail"]//li');
+$list_results = array();
+
+foreach ($list_articles as $article) {
+
+	$node = $xpath->query('descendant::span[@class="pufftext"]', $article);
+	$node_text = $node->item(0)->textContent;
+
+	foreach ($bad_words as $word) {
+		if ( stripos($node_text, $word) !== false ) {
+			array_push($frequencey, strtolower($word));
+
+			$result['span'] = $node->item(0)->textContent;
+
+			$node = $xpath->query('descendant::a/attribute::href', $l_article);
+			$result['a_href'] = $node->item(0)->value;
+
+			$node = $xpath->query('descendant::img/attribute::src', $l_article);
+			$result['img_src'] = $node->item(0)->value;
+
+			if ( array_key_exists ( $word , $list_results ) ) {
+				array_push($list_results[$word], $result);
+			} else {
+				$list_results[$word] = $result;
+			}
+		}
+	}
+}
+print_r($list_results);
+
 
 $frequencycount = array_count_values($frequencey);
 arsort($frequencycount);
